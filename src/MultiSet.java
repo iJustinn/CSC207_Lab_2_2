15ae4139b8b2cad21280fdc6a6276997eb40234d
Gordon <gordonchen2014@gmail.com>
2023-09-18 14:39:20 -0400

    abstract multiset

---
+++ src/MultiSet.java
@@ -0,0 +1,7 @@
+public abstract class MultiSet {
+    public abstract boolean add();
+    public abstract void remove();
+    public abstract boolean contains();
+    public abstract int is_empty();
+    public abstract int size();
+}
